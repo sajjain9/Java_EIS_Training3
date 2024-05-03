ef1fefdbd95b71e88733d44d41e95d8550910863
sajaljain <sajaljain@gmail.com>
2024-05-03 19:38:26 +0530

    First Commit : Main Branch : Calculate

---
+++ src/Calculate.java
@@ -0,0 +1,11 @@
+package src;
+
+class Calculate{
+    //sum : two integers
+    public static void sum(int a, int b){
+        System.out.println(a+b+10);
+    }
+    public static void main(String[] argv) {
+        sum(5,5);
+    }
+}
