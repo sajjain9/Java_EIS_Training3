162f03226ad208da299a28db6b8f00dc450d8fbc
sajaljain <sajaljain@gmail.com>
2024-05-06 11:35:33 +0530

    Method name changed.

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,16 +2,16 @@ package src;
 
 class Calculate{
     //sum : two integers
-    public static void sum(int a, int b){
+    public static void sum2(int a, int b){
         System.out.println(a+b+10);
         System.out.println("Adding two integers");
-        System.out.println("difference of two numbers:"+ sub(a,b));
+        System.out.println("difference of two numbers:"+ sub2(a,b));
     }
 
-    public static int sub(int a, int b){
+    public static int sub2(int a, int b){
         return a-b;
     }
     public static void main(String[] argv) {
-        sum(5,5);
+        sum2(5,5);
     }
 }
