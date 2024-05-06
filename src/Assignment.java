2cafdeede2fe8a55bdf2af10fa26e9e75b1575f9
sajaljain <sajaljain@gmail.com>
2024-05-06 11:14:21 +0530

    Ninth Commit : TestChild Branch : print(), sum()

--- src/Assignment.java
+++ src/Assignment.java
@@ -3,8 +3,13 @@ package src;
 public class Assignment {
 
     public static void print(){
-        System.out.println("Simple Method");
+        System.out.println("Simple Method from Test Child");
     }
+
+    public static void sum(int a, int b) {
+        System.out.println(a + b + 10);
+    }
+
     public static void main(String[] argv) {
         print();
     }
