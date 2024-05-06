2ad2bbf497dde4a12b843f27e648a0a44617c2f5
sajaljain <sajaljain@gmail.com>
2024-05-06 10:37:19 +0530

    Eighth Commit : Test Branch : main()

--- src/Assignment.java
+++ src/Assignment.java
@@ -5,4 +5,7 @@ public class Assignment {
     public static void print(){
         System.out.println("Simple Method");
     }
+    public static void main(String[] argv) {
+        print();
+    }
 }
