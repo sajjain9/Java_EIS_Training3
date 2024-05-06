ce7be28508055e0120c55bb840c4abf0db9fdbe5
sajaljain <sajaljain@gmail.com>
2024-05-06 11:28:09 +0530

    Ninth Commit : Sub method added.

--- src/Calculate.java
+++ src/Calculate.java
@@ -3,7 +3,13 @@ package src;
 class Calculate{
     //sum : two integers
     public static void sum(int a, int b){
-        System.out.println(a+b);
+        System.out.println(a+b+10);
+        System.out.println("Adding two integers");
+        System.out.println("difference of two numbers:"+ sub(a,b));
+    }
+
+    public static int sub(int a, int b){
+        return a-b;
     }
     public static void main(String[] argv) {
         sum(5,5);
